82e365caf2805e48b4e2341ae6703eddefbb5f0e
Egor <egosha.m73@gmail.com>
2021-04-09 13:44:20 +0300

    AddBook Purchase

--- client/src/Components/BoughtBook/BoughtBook.js
+++ client/src/Components/BoughtBook/BoughtBook.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function BoughtBook(props) {
   return (
-    <div>
+    <div className='background'>
     <div className='bookWindow'>
       <img src='' alt='' />
       <div className='bookInfo'>
         <h3 className='titleBook' >Название</h3>
-        <h5 className='autorBook' >Автор</h5>
+        <h5 className='authorBook' >Автор</h5>
         <div className='summaryBook' >Информация о книге</div>
       </div>
     </div>
--- client/src/Components/Purchase/Purchase.js
+++ client/src/Components/Purchase/Purchase.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 function Purchase(props) {
   return (
-    <div>
-      
+    <div className='background'>
+      <div className='bookWindow'>
+        <img src='' alt='' />
+        <div className='bookInfo'>
+          <h4 className='titleBook' >Название </h4>
+          <h6 className='authorBook' >Автор </h6>
+        </div>
+        <div className='formPurchase'>
+          Здесь форма оплаты картой 
+          {/* Сайт https://yoomoney.ru/quickpay/form */}
+        </div>
+      </div>
     </div>
   );
 }

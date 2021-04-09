59d8b60cb03182f20a7b23040f5d0aec98c5967e
Egor <egosha.m73@gmail.com>
2021-04-09 11:57:43 +0300

    BoughtBook PublishedBook InfoBook

--- client/src/Components/InfoBook/InfoBook.js
+++ client/src/Components/InfoBook/InfoBook.js
@@ -3,8 +3,19 @@ import React from 'react';
 function InfoBook(props) {
   return (
     <div>
-      
+    <div className='bookWindow'>
+      <img src='' alt='' />
+      <div className='bookInfo'>
+        <h3 className='titleBook' >Название</h3>
+        <h5 className='autorBook' >Автор</h5>
+        <div className='summaryBook' >Информация о книге</div>
+      </div>
     </div>
+    <div className='buttonList'>
+      <button className='button buttonBook' >Купить</button>
+      <button className='button buttonBook' >Пробный кусочек</button>
+    </div>
+  </div>
   );
 }
 

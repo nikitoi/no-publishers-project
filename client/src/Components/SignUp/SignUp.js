3d7a22204254d270a8b7ae38eb2ce4b1f835ec64
Nika Chervova <nikitoi@mail.ru>
2021-04-10 12:17:05 +0300

    login signup верстка

--- client/src/Components/SignUp/SignUp.js
+++ client/src/Components/SignUp/SignUp.js
@@ -3,13 +3,14 @@
 function SignUp(props) {
   return (
     <div>
-       <div className='background'>
+       <div className='background flex_center'>
         <div className='modal_form'>
-          <button className='button button-icon'></button>
-          <input className='auth' type='email' name='email' placeholder='Email'/>
-          <input className='auth' type='password' name='password' placeholder='Password'/>
-          <input className='auth' type='text' name='phone' placeholder='Phone number'/>
-          <button className='button button-entrance'>Зарегистрироваться</button>
+          {/* <button className='button button-icon'></button> */}
+          <div className="color_dark mb-1 font-12">Введите свои данные</div>
+          <input className='auth input mb-1' type='email' name='email' placeholder='Email'/>
+          <input className='auth input mb-1' type='password' name='password' placeholder='Password'/>
+          <input className='auth input mb-1' type='text' name='phone' placeholder='Phone number'/>
+          <button className='button button-entrance mb-1'>Регистрация</button>
         </div>
       </div>
     </div>

bd89c48fc1541fc297b9f7cad2688963b331a5b6
Azizbek <bazizbek@gmail.com>
2021-04-12 19:36:11 +0300

    upload book to store

--- client/src/Components/AddBook/AddBook.js
+++ client/src/Components/AddBook/AddBook.js
@@ -1,34 +1,80 @@
 import React, { useState } from 'react';
+import { useAuth } from "../../context/AuthContext";
+import firebase from 'firebase';
 import './AddBook.scss'
 
 function AddBook(props) {
 
+  const { uploadBook, currentUser } = useAuth();
+  const [url, setUrl] = useState('')
 
   function saveFile(e) {
-    console.log(e.target.file.files[0]);
-
-    let formData = new FormData()
-    formData.append('file', e.target.file.files[0])
-
-    fetch('http://localhost:4000/testupl', {
-      method: 'POST',
-      // headers: { 'Content-Type' : 'multipart/form-data' },
-      body: formData
-    })
-      .then(res => res.json())
-      .then(data => console.log(data))
     e.preventDefault()
+    // console.log(e.target.file.files[0]);
+
+    let pdfBack
+
+    const title = e.target.title.value
+    const description = e.target.description.value
+    const imgFile = e.target.image.files[0]
+    // const backFilename = pdfBack
+    const price = e.target.price.value
+    const pages = [1, 5]
+
+    const uploadTask = firebase.storage().ref(`books/${imgFile.name}`).put(imgFile);
+
+    uploadTask.on(
+      "state_changed",
+      (error) => {
+        // Error function ...
+        console.log(error);
+      },
+      () => {
+        // complete function ...
+        console.log('url vnizu')
+        firebase.storage()
+          .ref("books")
+          .child(imgFile.name)
+          .getDownloadURL()
+          .then((url) => {
+            setUrl(url);
+            // post image inside db
+            firebase.firestore().collection("posts").add({
+              imageUrl: url,
+              username: currentUser.displayName,
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+          });
+      }
+    );
+
+    // let formData = new FormData()
+    // formData.append('file', e.target.file.files[0])
+
+    // fetch('http://localhost:4000/testupl', {
+    //   method: 'POST',
+    //   // headers: { 'Content-Type' : 'multipart/form-data' },
+    //   body: formData
+    // })
+    //   .then(res => res.json())
+    //   .then(data => pdfBack = data)
+
+
+console.log(url);
+    console.log(title, description, imgFile, price);
+
+
   }
 
-  function pickImg(){
+  function pickImg() {
     document.querySelector('#img-input')?.click()
   }
 
-  function pickFile(){
+  function pickFile() {
     document.querySelector('#file')?.click()
   }
 
-  function showRules(){
+  function showRules() {
 
   }
 
@@ -37,19 +83,19 @@ function AddBook(props) {
     <div className='background  flex_center'>
       <form className='formAddBook modal_form' encType="multipart/form-data" method="post" action="/testupl" onSubmit={(e) => saveFile(e)}>
         <input className='auth input mb-1 wide-input' name='title' type='text' required placeholder='Название книги' />
-        <textarea className='auth input mb-1 input-textarea wide-input' name='summary' required type='text' placeholder='Описание' ></textarea>
+        <textarea className='auth input mb-1 input-textarea wide-input' name='description' required type='text' placeholder='Описание' ></textarea>
 
         <div className="flex_center add-book_add-file wide-input">
           <label htmlFor="file-input" className="color_dark mb-1 add-book_label">Выберите фото</label>
-          <input id="img-input" className="file_input" style={{display: "none"}} type="file" required name="image" />
+          <input id="img-input" className="file_input" style={{ display: "none" }} type="file" required name="image" />
           <input type="button" className="file_input input mb-1" id="loadFileXml" value="img" onClick={pickImg} />
         </div>
 
-        <div className="flex_center add-book_add-file wide-input">
+        {/* <div className="flex_center add-book_add-file wide-input">
           <label htmlFor="file-input" className="color_dark mb-1 add-book_label">Выберите файл</label>
-          <input id="file" className="file_input" style={{display: "none"}} type="file" required name="upload" accept="application/pdf" />
+          <input id="file" className="file_input" style={{display: "none"}} type="file" name="upload" accept="application/pdf" />
           <input type="button" className="file_input input mb-1" id="loadFileXml" value=".pdf" onClick={pickFile} />
-        </div>
+        </div> */}
 
         <input className='auth input mb-1 wide-input' name='price' type='number' required placeholder='Укажите ценник' />
         <div className="flex_column add-book_add-file wide-input">
@@ -64,6 +110,7 @@ function AddBook(props) {
       <div className="popup">
         <h4>Правила публикации</h4>
         <p>Мы не несем ответственности ни за что</p>
+        <img src={url} alt='img'/>
       </div>
     </div>
   );

26170c400d35116da495e85dfef38a9ab3be28e6
Egor <egosha.m73@gmail.com>
2021-04-12 11:52:23 +0300

    styles header logo slider

--- client/src/Components/MainDown/MainDown.js
+++ client/src/Components/MainDown/MainDown.js
@@ -1,38 +1,42 @@
 import React from 'react';
 import './MainDown.scss'
 import Carousel from 'react-bootstrap/Carousel'
+import zaglushka from '../BooksList/zaglushka'
+
 
 function MainDown(props) {
 
+  const carousels = []
+  for (let i = 0; i < Math.floor(zaglushka.length / 3) * 3; i += 3) {
+    carousels.push([zaglushka[i], zaglushka[i + 1], zaglushka[i + 2]])
+  }
+  console.log(carousels);
+
   return (
     <div className="maindown">
-      
-    <Carousel fade>
-  <Carousel.Item>
-    <div className="d-block w-100 blockBooks">
-    </div>
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-  <div className="d-block w-100 blockBooks">
-    </div>
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-  <div className="d-block w-100 blockBooks">
-    </div>
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+      <Carousel fade>
+        {carousels?.map(el => {
+          return (
+            <Carousel.Item>
+              <div className="d-block w-100 blockBooks">
+              </div>
+              <Carousel.Caption>
+                <div className='flex' >
+                  {el.map(el => {
+                    return (
+                      <div>
+                        <img className="slider-card_img" src={el.image} alt="book" />
+                        <h4 className="slider-card_title slider-text">{el.title}</h4>
+                        <h5 className="slider-card_author slider-text">{el.author}</h5>
+                      </div>
+                    )
+                  })}
+                </div>
+              </Carousel.Caption>
+            </Carousel.Item>
+          )
+        })}
+      </Carousel>
     </div>
   );
 }
